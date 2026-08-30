452ac92382e50489067dc1b519f324aefa199020
levalup <likier@sina.cn>
2026-08-30 19:38:28 +0800

    update test cases

--- tests/test_attributes.py
+++ tests/test_attributes.py
@@ -1,7 +1,7 @@
 import seetapsych_attributes as pkg
 
 
-def test_import_and_version():
+def test_version() -> None:
     print(f"Package: {pkg.__name__}")
     print(f"Version: {pkg.__version__}")
     assert isinstance(pkg.__version__, str)
